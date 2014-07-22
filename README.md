19c96907e4e7c4c5f8b5be7ac7e6b84470313043
rocky_manobi <tak.koroki@gmail.com>
2014-07-22 13:17:41 +0900

    README 追加

---
+++ README.md
@@ -0,0 +1,29 @@
+
+## Get Started
+
+### 前提
+
+* ArduinoがUSB接続されている
+* Firmataライブラリがインストールがインストールされている
+
+### 実行
+
+#### with GIT
+
+```
+git clone http://github.com/rockymanobi/arduino-with-javascript-sample.git
+cd arduino-with-javascript-sample
+npm install
+node sample
+```
+
+#### without GIT
+
+zipでダウンロード `URLURLURLURL`
+
+```
+cd ダウンロードしたディレクトリ
+cd arduino-with-javascript-sample
+npm install
+node sample
+```
--- sample.js
+++ sample.js
@@ -1,6 +1,5 @@
-var five = require("johnny-five"),
-    // or "./lib/johnny-five" when running from the source
-    board = new five.Board();
+var five = require("johnny-five");
+var board = new five.Board();
 
 board.on("ready", function() {
 
@@ -8,10 +7,8 @@ board.on("ready", function() {
   var pinNo = 2;
   var OUTPUT = 1;
 
-  // Set pin 13 to OUTPUT mode
   this.pinMode( pinNo, OUTPUT);
 
-  // Create a loop to "flash/blink/strobe" an led
   this.loop(100, function() {
     this.digitalWrite(pinNo, (val = val ? 0 : 1));
   });
